9166aa14e04eaabbb040f20df026abc3d2c28968
Kamii Akbari <ungewang@gmail.com>
2021-03-27 07:55:07 +0100

    Added past LEO orgin to Main

---
+++ kron-ui-framework/src/components/kron-box/kron-box.tsx
@@ -0,0 +1,24 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'kron-box',
+  styleUrl: 'kron-box.css',
+  shadow: true,
+})
+export class KronBox {
+
+@Prop() displaymode: string;
+
+  render() {
+    return (
+          
+  <div class={this.displaymode}>
+<slot>
+   
+</slot>
+  </div>
+
+    );
+  }
+
+}
